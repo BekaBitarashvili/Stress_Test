91a3589941ca9009ba0398fa506db0e87bbc1477
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-02-09 11:22:57 +0400

    Structure

---
+++ locust.py
@@ -0,0 +1,14 @@
+import time
+from locust import HttpUser, between, task
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        self.client.get(url="/")
+
+    @task
+    def view_item(self):
+        self.client.get(url="/customers/create")
