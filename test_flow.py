015faf1905ebf4d7e8b98475dac7f5e7a4033a51
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-02-15 18:53:23 +0400

    POST method variation

--- test_flow.py
+++ test_flow.py
@@ -1,21 +1,87 @@
 import time
 import random
 import string
+from _winapi import NULL
+
 from locust import HttpUser, between, task, TaskSet, constant
 
 
 class FirstApi(HttpUser):
-    wait_time = constant(1)
-    host = "https://reqres.in"
+    wait_time = constant(10)
+    host = "http://10.117.27.38:8000"
+    session_key = "F014373B-C969-45C4-9B2D-BB1336C845B6"
 
     @task
-    def create_user(self):
+    def login_user(self):
         payload = {
-            "name": "morpheus",
-            "job": "leader"
+            "account_id": "156507",
+            "session_key": "F24141E3-D32A-40CB-BD21-779B24C5069E",
+            "phone": "599****62"
         }
         headers = {
             "Content-Type": "application/json"
         }
-        response = self.client.post("/api/users", json=payload, headers=headers)
-        print(response.status_code)
+        time.sleep(10)
+        response = self.client.post("/api/login", json=payload, headers=headers)
+        print(f"{self.login_user.__name__} - {response.status_code}")
+
+    @task
+    def reset_password(self):
+        payload = {
+            "account_id": "90492",
+            "session_key": "2A8BDCCD-F7E9-42A9-BA52-FDCB14530214",
+            "phone": "599****03"
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        time.sleep(10)
+        response = self.client.post("/api/reset_password", json=payload, headers=headers)
+        print(f"{self.reset_password.__name__} - {response.status_code}")
+
+    @task
+    def update_password(self):
+        payload = {
+            "password": "Dato1234",
+            "re_enter_password": "Dato1234"
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key
+        }
+        time.sleep(10)
+        response = self.client.post("/api/update_password", json=payload, headers=headers)
+        print(f"{self.update_password.__name__} - {response.status_code} - {self.session_key}")
+
+    @task
+    def check_customer(self):
+        payload = {
+            "clientStatus": 1,
+            "customer": NULL,
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        time.sleep(10)
+        response = self.client.post("/api/check_customer", json=payload, headers=headers)
+        print(f"{self.check_customer.__name__} - {response.status_code}")
+
+    @task
+    def create_account(self):
+        payload = {
+            "message": "The lang field is required. (and 1 more error)",
+            "errors": {
+                "lang": [
+                    "The lang field is required."
+                ],
+                "client_status": [
+                    "The client status field is required."
+                ]
+            }
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        time.sleep(10)
+        response = self.client.post(url="/api/create_account", json=payload, headers=headers)
+        print(f"{self.create_account.__name__} - {response.status_code}")
