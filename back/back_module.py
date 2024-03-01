1cf2b47216c6c09c3fdcf2d0e6bf207097a92928
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-03-01 10:12:02 +0400

    Add New Services

--- back/back_module.py
+++ back/back_module.py
@@ -186,7 +186,7 @@ class SDA(HttpUser):
 class AltaLayer(HttpUser):
     wait_time = between(5, 10)
     host = "http://10.117.27.38:8000"
-    session_key = "CEE11FD3-15B9-4912-9A0A-1FB51C85C1D8"
+    session_key = "722151DE-77BB-4E01-8AD0-8E1378BADE2F"
 
     @task
     def get_loans(self):
@@ -211,5 +211,251 @@ class AltaLayer(HttpUser):
         response = self.client.get("/api/AltaLayer/get_currency", headers=headers)
         if response.status_code == 200:
             print(f'{self.get_currency.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_iso_list(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/AltaLayer/get_iso_list", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_iso_list.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_currency_rate(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/AltaLayer/get_currency_rate?FromCurrency=USD&ToCurrency=GEL", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_currency_rate.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def set_loan_name(self):
+        payload = {
+            "loanId": "3644521",
+            "Name_Of_Loan": "string"
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key
+        }
+        response = self.client.post("/api/AltaLayer/set_loan_name", headers=headers, json=payload)
+        if response.status_code == 200:
+            print(f'{self.set_loan_name.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+
+class OnlineContracts(HttpUser):
+    wait_time = between(5, 10)
+    host = "http://10.117.27.38:8000"
+    session_key = "722151DE-77BB-4E01-8AD0-8E1378BADE2F"
+
+    @task
+    def get_online_contracts(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/OnlineAgreement/get_contracts?AgreementTypeID=3&OnlineSignature=0&LoanID"
+                                   "=3575514", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_online_contracts.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+
+class Dashboard(HttpUser):
+    wait_time = between(5, 10)
+    host = "http://10.117.27.38:8000"
+    session_key = "722151DE-77BB-4E01-8AD0-8E1378BADE2F"
+
+    @task
+    def get_slider_images(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/dashboard/get_slider_images?lang=en", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_slider_images.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+
+class Akido(HttpUser):
+    wait_time = between(5, 10)
+    host = "http://10.117.27.38:8000"
+    session_key = "E6A2CBA5-C6E3-4D7E-A8CA-059F8BF3FE8A"
+
+    # username = DevTest ; password = QWEasd123 ; personal_id = 01001081951 ; application_id = 1261426
+
+    @task
+    def get_application_seen_time(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/Akido/get_application_seen_time?personal_id=01001081951&application_id="
+                                   "1261426", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_application_seen_time.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_application(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/Akido/get_application", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_application.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_active_application(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/Akido/get_active_application", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_active_application.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+
+class Akido2(HttpUser):
+    wait_time = between(5, 10)
+    host = "http://10.117.27.38:8000"
+    session_key = "C22E6DC5-8DA2-4A5F-B047-8EFB95F3550C"
+
+    @task
+    def send_token_for_contract(self):
+        payload = {
+            "application_id": "1261426"
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.post("/api/Akido/send_token_for_contract", headers=headers, json=payload)
+        if response.status_code == 200:
+            print(f'{self.send_token_for_contract.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def set_short_contract_agreement(self):
+        payload = {
+            "application_id": "1261426"
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.post("/api/Akido/set_short_contract_agreement", headers=headers, json=payload)
+        if response.status_code == 200:
+            print(f'{self.set_short_contract_agreement.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+    #
+    # @task
+    # def get_contract(self):
+    #     payload = {
+    #         "contract_type": "mainAgreementShort",
+    #         "application_id": "1261398"
+    #     }
+    #     headers = {
+    #         "Content-Type": "application/json",
+    #         "session-key": self.session_key,
+    #         "accept": "application/json"
+    #     }
+    #     response = self.client.post("/api/Akido/get_contract", headers=headers, json=payload)
+    #     if response.status_code == 200:
+    #         print(f'{self.get_contract.__name__} - {response.status_code}')
+    #     else:
+    #         print(f'{response.reason} - {response.status_code} - {response.text}')
+    #
+    # @task
+    # def verify_signature_token(self):
+    #     payload = {
+    #         "personal_id": "01001081951",
+    #         "application_id": "1261398",
+    #         "token": "000000"
+    #     }
+    #     headers = {
+    #         "Content-Type": "application/json",
+    #         "session-key": self.session_key,
+    #         "accept": "application/json"
+    #     }
+    #     response = self.client.post("/api/Akido/verify_signature_token", headers=headers, json=payload)
+    #     if response.status_code == 200:
+    #         print(f'{self.verify_signature_token.__name__} - {response.status_code}')
+    #     else:
+    #         print(f'{response.reason} - {response.status_code} - {response.text}')
+
+
+class User(HttpUser):
+    wait_time = between(5, 10)
+    host = "http://10.117.27.38:8000"
+    session_key = "E6A2CBA5-C6E3-4D7E-A8CA-059F8BF3FE8A"
+
+    @task
+    def get_user_login_history(self):
+        headers = {
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/user/get_user_login_history?page=1", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_user_login_history.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_login_user_info(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/user/get_login_user_info", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_login_user_info.__name__} - {response.status_code}')
+        else:
+            print(f'{response.reason} - {response.status_code} - {response.text}')
+
+    @task
+    def get_all_list(self):
+        headers = {
+            "Content-Type": "application/json",
+            "session-key": self.session_key,
+            "accept": "application/json"
+        }
+        response = self.client.get("/api/all_list", headers=headers)
+        if response.status_code == 200:
+            print(f'{self.get_all_list.__name__} - {response.status_code}')
         else:
             print(f'{response.reason} - {response.status_code} - {response.text}')
